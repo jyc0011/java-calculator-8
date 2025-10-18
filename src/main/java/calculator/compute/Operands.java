93ee05b45276d8c75242b765f56977b38f8c5419
jyc0011 <122250128+jyc0011@users.noreply.github.com>
2025-10-19 03:43:11 +0900

    feat: implement custom separator
    implement custom separator, and add some exception case

--- src/main/java/calculator/compute/Operands.java
+++ src/main/java/calculator/compute/Operands.java
@@ -8,7 +8,14 @@ public class Operands { // 연산자 추출
     public static Numbers from(String[] numberStrings) { // 문자열을 리스트로 변환
         List<Integer> numbers = new ArrayList<>();
         for (String numberStr : numberStrings) {
-            numbers.add(Integer.parseInt(numberStr));
+            if (numberStr.isEmpty()) {
+                continue;
+            }
+            try {
+                numbers.add(Integer.parseInt(numberStr));
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("입력값에 구분자를 제외하고 숫자가 아닌 문자(공백, 소수점 등)가 포함되었습니다.", e);
+            }
         }
         return new Numbers(numbers);
     }
